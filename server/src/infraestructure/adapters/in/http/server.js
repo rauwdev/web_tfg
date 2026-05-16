c2aef8eaa1dac74a8dba7d9c3258d98039738be8
Raúl <rauldumitru41@gmail.com>
2026-05-16 18:56:20 +0200

    fix: Solucion de "/{*splat}" de express5

--- server/src/infraestructure/adapters/in/http/server.js
+++ server/src/infraestructure/adapters/in/http/server.js
@@ -22,7 +22,7 @@ app.use("/api/user", userRouter)
 
 app.use(express.static(path.join(__dirname, "../../../../../client/dist")))
 
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
     res.sendFile(path.join(__dirname, "../../../../../client/dist/index.html"))
 })
 
