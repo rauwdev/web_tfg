f6a684f1237a9e412038ccd3db8952cf23f3367a
Raúl <rauldumitru41@gmail.com>
2026-05-16 22:26:57 +0200

    fix: Arreglados typos en la config del server y redireccion de vite

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ sequelize.authenticate()
     .then(() => {
         console.log("Conectado a la database")
         app.listen(PORT, () => {
-            console.log(`Servidor corriendo en http://localhost:${PORT}`)
+            console.log(`Servidor corriendo en http://13.36.174.103:${PORT}`)
         })
     })
     .catch(err => {
